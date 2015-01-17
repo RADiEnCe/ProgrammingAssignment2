210654821949e35cc5819c05f4a49a22bc8a97d9
Cedar <cedar.ren@gmail.com>
2015-01-17 11:45:43 +0800

    This is the first commit, with cachematrix.R all written up

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function makes a matrix with a cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## x caches the matrix itself
+        
+        inv <- NULL # the inversion cache
+        
+        ## get and set matrix
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() {
+                x
+        }
+        
+        ## get and set inversion
+        setinv <- function(inversion) {
+                inv <<- inversion
+        }
+        getinv <- function() {
+                inv
+        }
+        
+        ## return the functions
+        list(set = set, get = get,
+                            setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function inverses a matrix
+## made by the last function, checking
+## the cache for a shortcut
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # read the inverse cache
+        inverse <- x$getinv()
+        
+        ## check for validity and creat inv if needed
+        if(is.null(inverse)) {
+                matrix <- x$get()
+                inverse <- solve(x, ...)
+                x$setinv(inverse)
+        }
+        
+        ## return value
+        inverse
 }
